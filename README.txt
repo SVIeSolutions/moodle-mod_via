268aea6927cccc536b57fa97f6a9090e06450600
tfauquignon <thomas.fauquignon@sviesolutions.com>
2020-03-10 08:18:08 -0400

    Version 2020011401
    -Fix authentification from Via app
    -Integration of Moodle 3.8

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-Version 2018042005
+Version 2020011401
 
 /* for more information on the version please see mod/via/version.php */
 /* This plugin requires Via 8.7 or greater */
--- version.php
+++ version.php
@@ -30,8 +30,8 @@ defined('MOODLE_INTERNAL') || die();
 global $CFG;
 
 $plugin->component = 'mod_via';
-$plugin->version = 2018042005;
+$plugin->version = 2020011401;
 $plugin->requires = 2011033010;
 $plugin->cron     = 300;
 $plugin->maturity = MATURITY_STABLE; // This is considered as ready for production sites.
-$plugin->release = 'v3.4-r5';
+$plugin->release = 'v3.8-r1';
